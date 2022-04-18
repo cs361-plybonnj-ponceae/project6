7ad32c6e6b8bddfe2b6ded5c0603dbd25abede78
nplybon <plybonnj@dukes.jmu.edu>
2022-04-18 18:03:33 -0400

    termination tasks

--- project6.c
+++ project6.c
@@ -1,9 +1,7 @@
 /* CS 361 project6.c
-
-  Team: 
-  Names: 
-  Honor Code Statement: 
-
+  Team: 07
+  Names: Nic Plybon & Adrien Ponce
+  Honor Code Statement: This code complies with the JMU Honor Code.
 */
 
 #include <stdio.h>
@@ -22,9 +20,11 @@
 #include "classify.h"
 #include "intqueue.h"
 
+mqd_t tasks_mqd, results_mqd; // message queue descriptors
+
+    
 
 void *process_result(void *arg) {
-
     return NULL;
 
 }
@@ -34,12 +34,16 @@ int main(int argc, char *argv[])
     int input_fd;
     pid_t pid;
     off_t file_size;
-    struct mq_attr attr;
     char tasks_mq_name[16];
     char results_mq_name[18];
     struct task new_task;
     int num_clusters;
     pthread_t processor[NUM_THREADS];
+    struct mq_attr attributes;
+    attributes.mq_flags = 0;
+    attributes.mq_maxmsg = 1000;
+    attributes.mq_msgsize = MESSAGE_SIZE_MAX;
+    attributes.mq_curmsgs = 0;
 
     if (argc != 2) {
         printf("Usage: %s data_file\n", argv[0]);
@@ -71,7 +75,18 @@ int main(int argc, char *argv[])
         }
     }
 
-    for (int i = 0; i < NUM_THREADS; i++)
+
+
+    if ((tasks_mqd = mq_open(tasks_mq_name, O_RDWR | O_CREAT, 0600, &attributes)) < 0) {
+        printf("Error opening message queue %s: %s\n", tasks_mq_name, strerror(errno));
+        return 1;
+    }
+    if ((results_mqd = mq_open(results_mq_name, O_RDWR | O_CREAT, 0600, &attributes)) < 0) {
+        printf("Error opening message queue %s: %s\n", results_mq_name, strerror(errno));
+        return 1;
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++) 
         pthread_create(&(processor[i]), NULL, process_result, NULL);
     
     // Phase 1
@@ -79,6 +94,23 @@ int main(int argc, char *argv[])
     // Phase 2
 
     // Phase 3
-
+    struct task terminate;
+    terminate.task_type = TASK_TERMINATE;
+    for (int i = 0; i < NUM_PROCESSES; i++) {
+        // send to tasks queue
+        if (mq_send(tasks_mqd, (const char *) &terminate, sizeof(terminate), 0) < 0) {
+            printf("Error sending to tasks queue: %s\n", strerror(errno));
+            return 1;
+        }
+    }
+    //wait for children to terminate
+    wait(NULL);
+    //close any open mqds
+    mq_close(tasks_mqd);
+    mq_close(results_mqd);
+    //unlink mqueues
+    mq_unlink(tasks_mq_name);
+    mq_unlink(results_mq_name);
+    //terminates itself
     return 0;
 };
